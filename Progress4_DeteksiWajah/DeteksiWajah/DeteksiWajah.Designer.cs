9315ac35fdf5c716d5b958e6897592a930410f91
Riza Hardian <rizahardian@rocketmail.com>
2013-01-19 07:30:49 +0700

    commit
    sudah bisa menampilkan gambar wajah yang terdeteksi.

---
+++ Progress4_DeteksiWajah/DeteksiWajah/DeteksiWajah.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace deteksiwajah
+{
+    partial class DeteksiWajah
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeteksiWajah));
+            this.KameraImageBox = new Emgu.CV.UI.ImageBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.btnKamera = new System.Windows.Forms.Button();
+            this.btnGambar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbGambar = new System.Windows.Forms.PictureBox();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.KameraImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGambar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // KameraImageBox
+            // 
+            this.KameraImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.KameraImageBox.Location = new System.Drawing.Point(11, 11);
+            this.KameraImageBox.Margin = new System.Windows.Forms.Padding(2);
+            this.KameraImageBox.Name = "KameraImageBox";
+            this.KameraImageBox.Size = new System.Drawing.Size(476, 416);
+            this.KameraImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.KameraImageBox.TabIndex = 2;
+            this.KameraImageBox.TabStop = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // btnKamera
+            // 
+            this.btnKamera.Location = new System.Drawing.Point(495, 30);
+            this.btnKamera.Name = "btnKamera";
+            this.btnKamera.Size = new System.Drawing.Size(158, 36);
+            this.btnKamera.TabIndex = 7;
+            this.btnKamera.Text = "Dari Live Kamera";
+            this.btnKamera.UseVisualStyleBackColor = true;
+            this.btnKamera.Click += new System.EventHandler(this.btnKamera_Click);
+            // 
+            // btnGambar
+            // 
+            this.btnGambar.Location = new System.Drawing.Point(495, 72);
+            this.btnGambar.Name = "btnGambar";
+            this.btnGambar.Size = new System.Drawing.Size(160, 36);
+            this.btnGambar.TabIndex = 9;
+            this.btnGambar.Text = "Dari File Gambar";
+            this.btnGambar.UseVisualStyleBackColor = true;
+            this.btnGambar.Click += new System.EventHandler(this.btnGambar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(492, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Mode pengambilan data :";
+            // 
+            // pbGambar
+            // 
+            this.pbGambar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbGambar.Location = new System.Drawing.Point(495, 170);
+            this.pbGambar.Name = "pbGambar";
+            this.pbGambar.Size = new System.Drawing.Size(158, 195);
+            this.pbGambar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGambar.TabIndex = 11;
+            this.pbGambar.TabStop = false;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(495, 378);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(77, 48);
+            this.btnPrev.TabIndex = 12;
+            this.btnPrev.Text = "<<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(578, 377);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(77, 49);
+            this.btnNext.TabIndex = 13;
+            this.btnNext.Text = ">>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // DeteksiWajah
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 438);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.pbGambar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGambar);
+            this.Controls.Add(this.btnKamera);
+            this.Controls.Add(this.KameraImageBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "DeteksiWajah";
+            this.Text = "Aplikasi Pendeteksi Wajah";
+            this.Load += new System.EventHandler(this.DeteksiWajah_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.KameraImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGambar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox KameraImageBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button btnKamera;
+        private System.Windows.Forms.Button btnGambar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbGambar;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ Progress4_DeteksiWajah/DeteksiWajah/DeteksiWajah.cs
@@ -0,0 +1,184 @@
+﻿/*=================================================
+ *Kelompok 4
+ *[10109316] Riza Hardian
+ *[10109321] Mirah Anugraheny
+ *[10109326] Andri Nugraha Ramdhon
+ *[10109345] Fitrianingsih
+ *IF-8
+ *Sistem Multimedia
+ * ================================================== */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+
+
+namespace deteksiwajah
+{
+    public partial class DeteksiWajah : Form
+    {
+        //variabel global
+        private Capture tangkap; //mengambil gambar dari kamera dan menyimpan ke dalam frame-frame gambar
+        private HaarCascade haar; //deklarasi penggunaan kelas viola-jones untuk mendeteksi gambar 
+        Image<Bgr, Byte> FrameGambar;
+        Bitmap[] ExtWajah;
+        int NoWajah = 0;
+        public DeteksiWajah() 
+        {
+            InitializeComponent(); //inialisasi komponen
+        }
+
+        private void ProsesMenangkapFrame(object sender, EventArgs arg)
+        {
+            //menyimpan frame gambar yang ditangkap kamera ke imageframe
+            Image<Bgr, Byte> FrameGambar = tangkap.QueryFrame();
+            //mendeteksi wajah dari framegambar
+            if (FrameGambar != null) //mengkonfirmasi valid tidaknya framegambar
+            {
+                //konversi gambar ke gray-scale
+                Image<Gray, byte> framegrayscale = FrameGambar.Convert<Gray, byte>();
+
+                //mendeteksi wajah dari gray-scale image dan menyimpan ke dalam array wajah
+                var wajah = framegrayscale.DetectHaarCascade(haar, 1.1, 2,
+                                        HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                                        new Size(25, 25))[0];
+                //menandai kotak biru pada wajah yang dideteksi dari gambar
+                foreach (var wajah2 in wajah)
+                {
+                    FrameGambar.Draw(wajah2.rect, new Bgr(Color.Blue),3);
+                }
+            }
+            //menampilkan gambar pada EmguCV ImageBox
+            KameraImageBox.Image = FrameGambar.Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR, true);
+        }
+
+        private void btnKamera_Click(object sender, EventArgs e)
+        {
+            if (tangkap == null)
+            {
+                try
+                {
+                    tangkap = new Capture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+
+
+            if (tangkap != null)
+            {
+                if (btnKamera.Text=="Berhenti")
+                {
+                    btnKamera.Text = "Dari Live Kamera";
+                    Application.Idle -= ProsesMenangkapFrame;
+                }
+                else
+                {
+                    btnKamera.Text = "Berhenti";
+                    Application.Idle += ProsesMenangkapFrame;
+                }
+            }
+        }
+
+        private void MendeteksiWajah()
+        {
+            //menyimpan frame gambar yang ditangkap kamera ke imageframe
+            Image<Gray, byte> framegrayscale = FrameGambar.Convert<Gray, byte>();
+            //mendeteksi wajah dari gray-scale image dan menyimpan ke dalam array wajah
+            var wajah = framegrayscale.DetectHaarCascade(haar, 1.1, 1,
+                                    HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                                    new Size(25, 25))[0];
+            if (wajah.Length > 0)
+            {
+                //menampilkan jumlah wajah yang terdeteksi
+                MessageBox.Show("Total wajah yang terdeteksi: " + wajah.Length.ToString());
+                //deklarasi awal pembuatan ekstraksi wajah yang sudah ditangkap
+                Bitmap BmpInput = framegrayscale.ToBitmap();
+                Bitmap EkstraksiWajah;
+                Graphics KanvasWajah;
+                ExtWajah = new Bitmap[wajah.Length];
+                NoWajah = 0;
+                //menandai kotak biru pada wajah yang dideteksi dari gambar
+                foreach (var wajah2 in wajah)
+                {
+                    FrameGambar.Draw(wajah2.rect, new Bgr(Color.Blue), 3);
+                    //setting ukuran extraksiwajah yang akan menyimpan hasil data pendeteksian wajah
+                    EkstraksiWajah = new Bitmap(wajah2.rect.Width, wajah2.rect.Height);
+                    //set set kanvas yang nantinya akan digambarkan hasil dari ekstraksiwajah
+                    KanvasWajah = Graphics.FromImage(EkstraksiWajah);
+                    KanvasWajah.DrawImage(BmpInput, 0, 0, wajah2.rect, GraphicsUnit.Pixel);
+                    ExtWajah[NoWajah] = EkstraksiWajah;
+                    NoWajah++;
+                }
+                //menampilkan ekstraksiwajah ke dalam array extwajah[] 
+                pbGambar.Image = ExtWajah[0];
+                //menampilkan gambar ke kameraimagebox
+                KameraImageBox.Image = FrameGambar;
+            }
+        }
+
+        private void DeteksiWajah_Load(object sender, EventArgs e)
+        {
+            //mencari xml yang akan digunakan
+            haar = new HaarCascade("haarcascade_frontalface_alt_tree.xml");
+        }
+
+        private void btnGambar_Click(object sender, EventArgs e)
+        {
+            //mengambil data gambar dari direktori untuk di deteksi wajah
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Image InputGambar = Image.FromFile(openFileDialog.FileName);
+                FrameGambar = new Image<Bgr, byte>(new Bitmap(InputGambar));
+                KameraImageBox.Image = FrameGambar;
+                MendeteksiWajah();
+            }
+            //menyimpan gambar ke direktori c:\
+            FrameGambar.Save(@"C:\ScDariGambar.jpg");
+            //menampilkan notifikasi penyimpanan gambar
+            MessageBox.Show("Gambar telah disimpan di 'C:\' dengan nama 'ScDariGambar.jpg'");
+        }
+
+        //diskonek kamera
+        private void ReleaseData()
+        {
+            if (tangkap != null)
+                tangkap.Dispose();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (NoWajah > 0)
+            {
+                NoWajah--;
+                pbGambar.Image = ExtWajah[NoWajah];
+            }
+            else
+                MessageBox.Show("Ini gambar wajah pertama");
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (NoWajah < ExtWajah.Length - 1)
+            {
+                NoWajah++;
+                pbGambar.Image = ExtWajah[NoWajah];
+            }
+            else
+                MessageBox.Show("Ini gambar wajah terakhir");
+        }
+    }
+}    
+        
+
